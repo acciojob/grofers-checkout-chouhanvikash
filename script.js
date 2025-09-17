5e17962683c141bbbaf13de40fe0b85e5f24cc5f
chouhanvikash <96488608+chouhanvikash@users.noreply.github.com>
2025-09-17 20:10:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,25 @@ document.body.appendChild(getSumBtn);
 
 const getSum = () => {
 //Add your code here
+	let items=document.querySelectorAll(".price");
+	let arr=Array.from(items);
+	let sum=0;
+	for(let i=0;i<arr.length;i++)
+		{
+			sum+=arr[i].textContent;
+		}
+	let table = document.getElementsByTagName("table")[0];
+let tbody = table.tBodies[0];
+
+// Insert a new row at the end
+let row = tbody.insertRow();
+
+// Insert a new cell in that row (first column)
+let cell = row.insertCell(0);
+
+// Put some text inside the cell
+cell.textContent = sum;  // assuming sum is defined somewhere
+
   
 };
 
